37627a8f44e486783896763aabe9a253c79a78fc
Yuheng Ouyang <yuhengo@students.cs.ubc.ca>
2024-11-26 02:39:52 -0800

    Correct SQL table name to allow queries

--- src/query.py
+++ src/query.py
@@ -22,7 +22,7 @@ try:
     cursor = connection.cursor()
 
     # Execute the query
-    query = "SELECT * FROM UserExpense WHERE user_id = %s"
+    query = "SELECT * FROM Expense WHERE user_id = %s"
     cursor.execute(query, (args.user_id,))
     rows = cursor.fetchall()
 
